084f844aeb2fb9e80a1b76bdf0f0009e8462705b
RDF014 <rdf014@gmail.com>
2017-01-28 18:56:23 -0800

    Up to arrays done

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,13 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-  // console.log(obj);
-  console.log('"' + arguments[0][0] + '"')
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (obj.constructor === Array) {
+    if (obj.length > 0) {
+      var stringifyArr = [];
+      for (var i = 0; i < obj.length; i++) {
+        stringifyArr.push( stringifyJSON(obj[i]) );
+      }
+      return '[' + stringifyArr + ']';
+    } else if (obj.length === 0){
+      return '[]';
+    }
+  }
+  return '' + obj + '';
 };
 
-stringifyJSON({});                  // '{}'
-stringifyJSON(true);                // 'true'
-stringifyJSON('foo');               // '"foo"'
-stringifyJSON([1, 'false', false]); // '[1,"false",false]'
-stringifyJSON({ x: 5 });            // '{"x":5}'
+console.log(stringifyJSON({}) );                  // '{}'                 Object
+console.log(stringifyJSON([8,5,6]) );                // 'true'               Boolean
+
+
+console.log(stringifyJSON([1, 'false', false]) ); // '[1,"false",false]'  Array
+console.log(stringifyJSON({ x: 5 }) );            // '{"x":5}'            Object
+
+// console.log(JSON.stringify(9000));
+// console.log(JSON.stringify('foo'));
